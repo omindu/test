e3df85831f74560a75c16e3ebb27f046b85be31b
Omindu Rathnaweera <omindu.dishan@gmail.com>
2020-10-25 16:08:50 +0530

    Create builder.sh

---
+++ builder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash +x
+wget https://raw.githubusercontent.com/mevan-karu/pom_version_property_finder/master/src/version_property_finder.py
+echo ""
+echo "=========================================================="
+echo "    PR_LINK: $PR_LINK"
+
+USER=$(echo $PR_LINK | awk -F'/' '{print $4}')
+REPO=$(echo $PR_LINK | awk -F'/' '{print $5}')
+PULL_NUMBER=$(echo $PR_LINK | awk -F'/' '{print $7}')
+PR_LINK=${PR_LINK%/}
+
+# ::set-output name={name}::{value}
+
+echo "    USER: $USER"
+echo "    REPO: $REPO"
+echo "    PULL_NUMBER: $PULL_NUMBER"
+echo "::set-output name=REPON::$REPO"
+echo "=========================================================="
